b0e9e94c2435db2181fc2029c4ec7a8c92361ffc
YoungWon <nbbb9@naver.com>
2024-05-27 10:22:47 +0900

    first commit

---
+++ src/main/java/com/encore/outpick_backend/OutpickBackendApplication.java
@@ -0,0 +1,13 @@
+package com.encore.outpick_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutpickBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutpickBackendApplication.class, args);
+	}
+
+}
