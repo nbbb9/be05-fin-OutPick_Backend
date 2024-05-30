4bf21f307cb33ae859bdcb0253a87f11a88a2a1f
kyungwon2won2 <ykw1230@naver.com>
2024-05-30 16:28:59 +0900

    add : 최여경원 최종수정

--- src/main/java/com/encore/outpick_backend/Analyze/controller/AnalyzeController.java
+++ src/main/java/com/encore/outpick_backend/Analyze/controller/AnalyzeController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,20 +15,19 @@ import java.util.List;
 
 @Tag(name = "통계/분석", description = "통계/분석과 관련된 api 목록")
 @RestController
-@RequestMapping("/sales")
+@RequestMapping
 public class AnalyzeController {
     @Autowired
     private AnalyzeService analyzeService;
 
     // 회사의 월별 판매량 분석
     @Operation(summary = "회사의 월별 총 판매량" , description = "회사의 전체매출을 불러오는 api")
-    @GetMapping
-    public List<AnalyzeEntireVO> get_analyze_entire(){
+    @GetMapping("/sales/{year}")
+    public List<AnalyzeEntireVO> get_analyze_entire(@PathVariable("year") int year){
 
-        return analyzeService.get_analyze_entire();
+        return analyzeService.get_analyze_entire(year);
     }
 
-//    @GetMapping("/{month}")
 
 
 }
