4c22fa9143b668e75fd02fb8780c0c5cade97ea3
kyungwon2won2 <ykw1230@naver.com>
2024-05-30 11:29:19 +0900

    add : 건의ë문 최종수정, 분석-회사의 월별 총판매량 추가

--- src/main/java/com/encore/outpick_backend/Proposal/domain/ProposalDTO.java
+++ src/main/java/com/encore/outpick_backend/Proposal/domain/ProposalDTO.java
@@ -1,16 +1,19 @@
 package com.encore.outpick_backend.Proposal.domain;
 
 import lombok.Data;
-import java.util.Date;
+
+import java.time.LocalDateTime;
 
 @Data
 public class ProposalDTO {
     private int proposal_id;
     private int shop_id;
+    private String shop_name;
     private String title;
     private String content;
-    private Date date;
+    private LocalDateTime date;
     private ProposalCategory category;
     private String completed;
     private String solution;
+    private String employee_name;
 }
