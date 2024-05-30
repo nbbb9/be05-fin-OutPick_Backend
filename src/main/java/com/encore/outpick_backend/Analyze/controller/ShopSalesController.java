40b5426060ca2ff969e5325102a3ba006c066b52
seungho <qswa97@gmail.com>
2024-05-30 16:24:05 +0900

    테스트 및 수정 완료!

--- src/main/java/com/encore/outpick_backend/Analyze/controller/ShopSalesController.java
+++ src/main/java/com/encore/outpick_backend/Analyze/controller/ShopSalesController.java
@@ -6,6 +6,8 @@ import com.encore.outpick_backend.Analyze.service.ShopSalesService;
 import com.encore.outpick_backend.Login.controller.LoginController;
 import com.encore.outpick_backend.Login.domain.LoginDTO;
 import com.encore.outpick_backend.StockRequest.domain.StockRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/sales")
+@Tag(name="매장별 월 판매량")
 public class ShopSalesController {
 
     private final ShopSalesService shopSalesService;
@@ -23,6 +26,7 @@ public class ShopSalesController {
 
 
     // 매장별 월 판매량을 조회할 수 있다.
+    @Operation(summary = "매장별 월 판매량", description = "년도와 월을 입력하여 매장별 월 판매량을 조회할 수 있다.")
     @GetMapping("/{year}/{month}")
     public ResponseEntity<List<ShopSalesDTO>> read_list(@PathVariable("year") int year, @PathVariable("month") int month,
                                                         @RequestHeader("login_token") String token){
