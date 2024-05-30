c2cc449c2b7eb68679065a299d4d8ebcd676a9ec
kyungwon2won2 <ykw1230@naver.com>
2024-05-30 14:01:01 +0900

    add : 건의ë문 최종수건의문, 분석일부 최종

--- src/main/java/com/encore/outpick_backend/Proposal/domain/ProposalCategory.java
+++ src/main/java/com/encore/outpick_backend/Proposal/domain/ProposalCategory.java
@@ -4,20 +4,3 @@ public enum ProposalCategory {
 
     상품, 운영방식, 인력
 }
-
-
-//public enum Category {
-//    PRODUCT("상품"),
-//    OPERATION_METHOD("건의"),
-//    PERSONNEL("인력");
-//
-//    private final String korean;
-//
-//    Category(String korean) {
-//        this.korean = korean;
-//    }
-//
-//    public String getKorean() {
-//        return korean;
-//    }
-//}
