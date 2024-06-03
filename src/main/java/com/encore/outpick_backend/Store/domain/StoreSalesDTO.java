d92ce0f22f6e8529cfca1882a8d2bbe6cb4ca81e
YoungWon <nbbb9@naver.com>
2024-06-03 09:35:04 +0900

    modify : StoreSales~ & StoreStock~

--- src/main/java/com/encore/outpick_backend/Store/domain/StoreSalesDTO.java
+++ src/main/java/com/encore/outpick_backend/Store/domain/StoreSalesDTO.java
@@ -2,12 +2,14 @@ package com.encore.outpick_backend.Store.domain;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class StoreSalesDTO {
 
     private int shop_sales_id;//매장판매 ID
-    private int shop_id;//매장 ID
-    private int product_id;//상품ID
+    private int product_name;//상품이름
     private int quantity;//판매량(수량)
+    private Date date;//판매일
 
 }
