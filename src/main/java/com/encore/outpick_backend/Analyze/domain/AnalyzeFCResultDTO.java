0d0d03cc6fe7443f2130d6b4af6180b1b84ad2bf
seungho <qswa97@gmail.com>
2024-06-17 18:21:30 +0900

    1차 분류 수정 완료.

---
+++ src/main/java/com/encore/outpick_backend/Analyze/domain/AnalyzeFCResultDTO.java
@@ -0,0 +1,11 @@
+package com.encore.outpick_backend.Analyze.domain;
+
+import lombok.Data;
+
+@Data
+public class AnalyzeFCResultDTO {
+
+    private int sales_quantity;
+    private String first_classification;
+
+}
--- src/main/java/com/encore/outpick_backend/Analyze/service/AnalyzeShopSalesService.java
+++ src/main/java/com/encore/outpick_backend/Analyze/service/AnalyzeShopSalesService.java
@@ -1,6 +1,7 @@
 package com.encore.outpick_backend.Analyze.service;
 
 import com.encore.outpick_backend.Analyze.domain.*;
+import com.encore.outpick_backend.Analyze.mapper.AnalyzeMapper;
 import com.encore.outpick_backend.Analyze.mapper.AnalyzeShopSalesMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,10 +19,17 @@ public class AnalyzeShopSalesService {
         return analyzeShopSalesMapper.read_list(year, month);
     }//read_list end
 
-    public List<AnalyzeFCResponseDTO> read_fc_list(AnalyzeFCRequestDTO analyzeFCRequestDTO){
+    public AnalyzeFCResponseDTO read_fc_list(AnalyzeFCRequestDTO analyzeFCRequestDTO){
         log.info("read_fc_list Service");
+        List<AnalyzeFCResultDTO> lists = analyzeShopSalesMapper.read_fc_list(analyzeFCRequestDTO);
 
-        return analyzeShopSalesMapper.read_fc_list(analyzeFCRequestDTO);
+        AnalyzeFCResponseDTO result = new AnalyzeFCResponseDTO();
+        for(AnalyzeFCResultDTO list : lists) {
+            result.getSales_quantity().add(list.getSales_quantity());
+            result.getFirst_classification().add(list.getFirst_classification());
+
+        }
+        return result;
     }//read_fc_list end
 
     public List<AnalyzeSCResponseDTO> read_sc_list(AnalyzeSCRequestDTO analyzeSCRequestDTO){
