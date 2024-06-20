034e8193ad4e9ef64ce0f66c2b3ee71a04ba6e55
noon <reallyjelly@gmail.com>
2024-06-20 11:56:00 +0900

    add analyze product list

--- src/main/java/com/encore/outpick_backend/Analyze/mapper/AnalyzeMapper.java
+++ src/main/java/com/encore/outpick_backend/Analyze/mapper/AnalyzeMapper.java
@@ -5,6 +5,8 @@ import com.encore.outpick_backend.Analyze.domain.AnalyzeFCRequestDTO;
 import com.encore.outpick_backend.Analyze.domain.AnalyzeFCResponseDTO;
 import com.encore.outpick_backend.Analyze.domain.AnalyzePriceRequestDTO;
 import com.encore.outpick_backend.Analyze.domain.AnalyzePriceResponseDTO;
+import com.encore.outpick_backend.Analyze.domain.AnalyzeProductRequestDTO;
+import com.encore.outpick_backend.Analyze.domain.AnalyzeProductResponseDTO;
 import com.encore.outpick_backend.Analyze.domain.AnalyzeSCRequestDTO;
 import com.encore.outpick_backend.Analyze.domain.AnalyzeSCResponseDTO;
 
@@ -18,4 +20,5 @@ public interface AnalyzeMapper {
     public List<AnalyzeFCResponseDTO> read_fc_list(AnalyzeFCRequestDTO analyzeFCRequestDTO);
     public List<AnalyzeSCResponseDTO> read_sc_list(AnalyzeSCRequestDTO analyzeSCRequestDTO);
     public List<AnalyzePriceResponseDTO> read_price_list(AnalyzePriceRequestDTO analyzePriceRequestDTO);
+    List<AnalyzeProductResponseDTO> read_product_list(AnalyzeProductRequestDTO analyzeProductRequestDTO);
 }
